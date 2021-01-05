6a66814acedafd126070965f40158428e1c225f8
Haruki Kondo <haruki-kondo@secom.co.jp>
2021-01-05 18:47:10 +0900

    Room.solとRoomFactory.solを実装したので、コミットします。

--- contracts/Room.sol
+++ contracts/Room.sol
@@ -1,1 +1,30 @@
 pragma solidity ^0.7.4;
+
+// openaeppelenin-solidityファイルを読み込む
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+// Roomコントラクト
+contract Room is Ownable {
+
+    // デプロイ用コンストラクター
+    // @param _creator ルーム作成者のアドレス
+    constructor(address _creator) {
+        owner = _creator;
+    }
+
+    // 預金をRoomコントラクトへ送金する関数
+    // 送金を可能とするためにpayable修飾子をつける
+    function deposit() external payable {}
+
+    // 報酬をRoomコントラクトへ送金する関数
+    // @param _reward 報酬
+    // @param _dest 投稿者のアドレス
+    function sendReward(uint256 _reward, address _dest) external onlyOwner{
+        _dest.transfer(_reward);
+    }
+
+    // コントラクトで余ったetherをオーナーへ返金する関数
+    function refundToOwner() external onlyOwner {
+        owner.transfer(address(this).balance);
+    }
+}
--- contracts/RoomFactory.sol
+++ contracts/RoomFactory.sol
@@ -1,1 +1,14 @@
 pragma solidity ^0.7.4;
+
+// Room.solファイルを読み込む
+import "./Room.sol";
+
+//RoomFactoryコントラクト
+contract RoomFactory {
+
+    //Roomコントラクトのコンストラクターを実行する関数
+    function createRoom() external payable {
+        // Roomコントラクトを指定してEthereumネットワークにコンストラクターをデプロイする
+        address newRoom = (new Room).value(msg.value)(msg.sender);
+    }
+}
